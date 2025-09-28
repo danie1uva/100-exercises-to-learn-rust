ef7e64d427ddf671b3f662dcdb110fcb668b50fa
Daniel Goodwin <daniel.goodwin@student.uva.nl>
2025-09-28 02:45:16 +0200

    doing bits

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,21 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let mut sequence: Vec<u32> = vec![0,1];
+    if n == 0 {
+        0
+    }
+    else if n == 1 {
+        1
+    }
+    else {
+        while sequence.len() <= (n as usize) {
+            let seq_len = sequence.len();
+            sequence.push(sequence[seq_len-1] + sequence[seq_len-2]);
+        }
+        let seq_len = sequence.len();
+        sequence[seq_len-1]
+    }
 }
 
 #[cfg(test)]
